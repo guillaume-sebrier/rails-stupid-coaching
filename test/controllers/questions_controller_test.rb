07d1b24dd23cc2e284f3030d16bea184e96744c6
guillaume sebrier <guillaume.sebrier@gmail.com>
2020-11-09 17:38:29 +0100

    test

--- test/controllers/questions_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class QuestionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get ask" do
-    get questions_ask_url
-    assert_response :success
-  end
-
-end
